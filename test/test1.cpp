a9f9821592ea03a055cebea62ba3830e43ed1551
Jakub Mierzwa <kontakt.jakubmierzwa@gmail.com>
2023-11-19 20:22:46 +0100

    [#7] Add unit test for CSVImporter class

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,6 +1,22 @@
 #include <gtest/gtest.h>
+#include "../src/DataImporter.hpp"
 
-TEST(ExampleTest, Example)
+class CSVImporterTest : public ::testing::Test
 {
-    EXPECT_EQ(1, 1);
+protected:
+    CSVImporter importer;
+};
+
+TEST_F(CSVImporterTest, ImportDataFromValidFile)
+{
+    importer.importData("../../data.csv");
+    auto data = importer.getData();
+    EXPECT_FALSE(data.empty());
+}
+
+TEST_F(CSVImporterTest, HandleNonexistentFile) 
+{
+    importer.importData("nonexistent.csv");
+    auto data = importer.getData();
+    EXPECT_TRUE(data.empty());
 }
