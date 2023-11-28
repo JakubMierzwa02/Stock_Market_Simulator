18931f880d7c50812afc5690e222246552015404
Jakub Mierzwa <kontakt.jakubmierzwa@gmail.com>
2023-11-28 21:40:51 +0100

    [#29] Add unit tests for DataValidator class

--- test/test1.cpp
+++ test/test1.cpp
@@ -7,6 +7,12 @@ protected:
     importer::DataImporter importer;
 };
 
+class DataValidatorTest : public ::testing::Test
+{
+protected:
+    importer::DataValidator validator;
+};
+
 TEST_F(DataImporterTest, ImportDataFromValidFile)
 {
     importer.importData("../../data.csv", "CSV");
@@ -17,4 +23,23 @@ TEST_F(DataImporterTest, HandleNonexistentFile)
     importer.importData("nonexistent.csv", "CSV");
     auto data = importer.getData();
     EXPECT_TRUE(data.empty());
+}
+
+TEST_F(DataValidatorTest, ValidateDate)
+{
+    EXPECT_TRUE(validator.validateDate("2023-01-01"));
+    EXPECT_FALSE(validator.validateDate("01-01-2023"));
+    EXPECT_FALSE(validator.validateDate("2023/01/01"));
+}
+
+TEST_F(DataValidatorTest, ValidatePrice) 
+{
+    EXPECT_TRUE(validator.validatePrice(100.50));
+    EXPECT_FALSE(validator.validatePrice(-1.00));
+}
+
+TEST_F(DataValidatorTest, ValidateVolume) 
+{
+    EXPECT_TRUE(validator.validateVolume(1000));
+    EXPECT_FALSE(validator.validateVolume(-100));
 }
